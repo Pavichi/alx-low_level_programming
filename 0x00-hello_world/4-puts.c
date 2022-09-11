4e1e04fe4326ca2aa302ab4b6944cd7164994dd2
root <chioma.patrick.241215@unn.edu.ng>
2022-09-11 04:16:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <studio.h>
 
 /**
- * *main - Entry point
- * *
- * *Return: Always 0*/
+*main - Entry point
+*
+*Return: Always 0
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
 }
