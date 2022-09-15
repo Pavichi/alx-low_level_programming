3fb5f9cf68274dbf41807cd0538ceb66f6e649c1
root <chioma.patrick.241215@unn.edu.ng>
2022-09-14 19:12:36 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,12 @@
-1. Write a program that prints _putchar, followed by a new line.
-2. Write a function that prints the alphabet, in lowercase, followed by a new line.
-3. Write a function that prints 10 times the alphabet, in lowercase, followed by a new line
-4. Write a function that checks for lowercase character
-5. Write a function that checks for alphabetic character
-6. Write a function that prints the sign of a number.
-7. Write a function that computes the absolute value of an integer.
-8. Write a function that prints the last digit of a number.
-9. Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
-10. Write a function that prints the 9 times table, starting with 0.
-11.Write a function that adds two integers and returns the result.
-12.Write a function that prints all natural numbers from n to 98, followed by a new line.
-
+==> File: 0-putchar.c is a program that prints -putchar, followed by a new line.
+==> File: 1-alphabet.c is a function that prints the alphabet, in lowercase, followed by a new line.
+==> File: 2-print_alphabet_x10.c is a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+==> File: 3-islower.c is a function that checks for lowercase character.
+==> File: 4-isalpha.c is a function that checks for alphabetic character.
+==> File 5-sign.c is a function that prints the sign of a number.
+==> File 6-abs.c is a function that computes the absolute value of an integer.
+==> File 7-print_last_digit.c is a function that prints the last digit of a number.
+==> File 8-24_hours.c is a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+==> File 10-add.c is a function that adds two integers and returns the result.
+==> File 11-print_to_98.c is a function that prints all natural numbers from n to 98, followed by a new line.
+File: `_putchar.c` contains the _putchar() function definition.
