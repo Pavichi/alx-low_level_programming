458cad2998a8c7bef21d4a6f5d7d28ddcaeab36b
root <chioma.patrick.241215@unn.edu.ng>
2022-09-15 12:37:20 -0700

    0-main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,13 @@
--putchar.c is a program that prints -putchar, followed by a new line.
--alphabet.c is a function that prints the alphabet, in lowercase, followed by a new line.
--print_alphabet_x10.c is a function that prints 10 times the alphabet, in lowercase, followed by a new line.
--islower.c is a function that checks for lowercase character.
--isalpha.c is a function that checks for alphabetic character.
--sign.c is a function that prints the sign of a number.
--abs.c is a function that computes the absolute value of an integer.
--print_last_digit.c is a function that prints the last digit of a number.
--24_hours.c is a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
--add.c is a function that adds two integers and returns the results.
--print_to_98.c is a function that prints all natural numbers from n to 98, followed by a new line.
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
