fe7f5902c249ac9b7cf1a0d1adf8dfa260f3a798
root <chioma.patrick.241215@unn.edu.ng>
2022-10-05 15:55:17 -0700

    Task 0

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,17 +1,11 @@
-#include <stdio.h>
 #include "main.h"
-/**
- * _isupper - checks if a character is
- * uppercase or lowercase
- * @c: character
- *
- * Return: 1 if uppercase and 0 if not
- */
 
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+	return (c >= 'A' && c <= 'Z');
 }
