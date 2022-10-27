c33af33e96c60545c8b938b862cde5a87520e0a2
root <chioma.patrick.241215@unn.edu.ng>
2022-10-27 11:44:20 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - little or big endian
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int bit = 0x01;
+
+	if (((bit << 4) & 0x1) == 0x1)
+		return (0);
+	else
+		return (1);
+
+}
